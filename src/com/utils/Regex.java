34dfbef34cc8121dc72e7d3f86539d0ef090b67d
kdesgros <kdesgros@VIEWW7-2016-3.isimtl.local>
2017-07-18 12:14:55 -0400

    Ajouter une fonction regex.

--- src/com/utils/Regex.java
+++ src/com/utils/Regex.java
@@ -1,24 +1,32 @@
 package com.utils;
 
-import java.util.regex.Pattern;
-
-// TO DO
+import java.util.regex.*;
 
 public class Regex {
 
-// 	Password Validation
-//	- at least 8 characters
-//	- must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
-//	- Can contain special characters	
-//	"/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm";
-
-	public static boolean regexChecker(String theRegex, String str2Check){
+	// 	Password Validation
+	//	Minimum eight characters, at least one letter and one number:
+	private static String passwordValidation = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+	
+	public static void regexChecker(String theRegex, String str2Check){
 		boolean passwordIsValid = false;
 		
 		Pattern checkRegex = Pattern.compile(theRegex);
-		
-		
-		return passwordIsValid;
+		Matcher regexMatcher = checkRegex.matcher( str2Check );
+				
+		// Cycle through the positive matches and print them to screen
+		// Make sure string isn't empty and trim off any whitespace
+				
+		while ( regexMatcher.find() ){
+			if (regexMatcher.group().length() != 0){
+				System.out.println( regexMatcher.group().trim() );
+						
+				// You can get the starting and ending indexs	
+				System.out.println( "Start Index: " + regexMatcher.start());
+				System.out.println( "Start Index: " + regexMatcher.end());
+				}
+			}
+				
+		System.out.println();
 	}
-
 }
