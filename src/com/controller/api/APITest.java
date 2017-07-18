0b17ec13c2e66324b036f4a6cd429cffb33f5dd6
kdesgros <kdesgros@VIEWW7-2016-3.isimtl.local>
2017-07-18 15:34:03 -0400

    Added the API

---
+++ src/com/controller/api/APITest.java
@@ -0,0 +1,34 @@
+package com.controller.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class APITest {
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String ressoureXML(){
+		String ressource = null;
+		
+		return ressource;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ressoureJSON(){
+		String ressource = null;
+		
+		return ressource;
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String ressoureHTML(){
+		String ressource = null;
+		
+		return ressource;
+	}
+}
