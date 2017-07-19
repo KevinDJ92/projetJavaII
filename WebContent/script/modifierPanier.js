b023e2d9a3b837c6338779338f912daad0215281
kdesgros <kdesgros@VIEWW7-2016-3.isimtl.local>
2017-07-19 13:44:33 -0400

    travailler sur modifier panier

--- WebContent/script/modifierPanier.js
+++ WebContent/script/modifierPanier.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
   
-    // TO DO
-    document.getElementById('ajouterProduit').addEventListener('change', function(){
+    var nom;
+    document.getElementById('ajouterProduit').addEventListener('change', function(e){
     	if (typeof(Storage) !== "undefined") {    
+    		nom = e.previousSibling.src;
     		
     		var valeur = localStorage.getItem(nom);
     		valeur++;
-    		localStorage.setItem(nom);
+    		localStorage.setItem(valeur);
     	}
     });
+    
     document.getElementById('retirerProduit').addEventListener('change', function(){
     	if (typeof(Storage) !== "undefined") {    
+    		nom = e.previousSibling.src;
     		
     		var valeur = localStorage.getItem(nom);
     		valeur++;
