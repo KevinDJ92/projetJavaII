a8a989c4467ebb5cf083a970c9bd60323deff7d3
ahmed <ahmed@LAPTOP-ESUEAN8U.isimtl.local>
2017-07-26 18:28:07 -0400

    envoi email

--- src/com/controller/AjoutAchat.java
+++ src/com/controller/AjoutAchat.java
@@ -18,6 +18,7 @@ import com.entities.Achat;
 import com.entities.LigneCommande;
 import com.entities.Utilisateur;
 import com.utils.Constante;
+import com.utils.GestionMail;
 
 /**
  * Servlet implementation class AjoutAchat
@@ -35,25 +36,38 @@ public class AjoutAchat extends HttpServlet {
 		ArrayList<Achat> achats = new ArrayList();
 		
 		HashMap<Integer,LigneCommande> monPanier=(HashMap<Integer,LigneCommande>)request.getSession().getAttribute("LePanier");
+		
+		Utilisateur util = ((Utilisateur)request.getSession().getAttribute(Constante.clefSession));
+		String msg = "<h1>Merci d'avoir magaziner chez nous</h1>";
+		String to = util.getEmail();
+		String sujet = "Confirmation de votre achat";
 		for(Map.Entry<Integer, LigneCommande> entry : monPanier.entrySet()){
 			
 				
-				Utilisateur util = ((Utilisateur)request.getSession().getAttribute(Constante.clefSession));
+				
                 Achat achat = new Achat(util.getId(),entry.getValue().getProduit().getId(),
              		   			"abc123",null,"dfsdf","envoyer",entry.getValue().getQuantite());
                
                 achats.add(achat);
+                msg+="<p>nom de l'article " +entry.getValue().getProduit().getNom() +"</p>\r\n" + 
+						"	                    	<p>La quantité de votre commande est :"+entry.getValue().getQuantite()+"</p>\r\n" + 
+						"	                    	<p>Le prix total pour cette article est "+entry.getValue().getQuantite()+" * "+entry.getValue().getProduit().getPrix()+" = "+entry.getValue().getQuantite()*entry.getValue().getProduit().getPrix()+"$</p>\r\n" + 
+						"	                  <hr color=\"black\"> ";
 			
 			
 		}
 		
 		System.out.println(achats);
 		if(achats != null) {
-			for(Achat achat : achats) {
-				System.out.println("tester ajout achat idUtil "+ achat.getIdUtil());
-				AchatAction.ajoutAchat(achat);
-			}
 			
+			
+			for(Achat achat : achats) {
+				
+				AchatAction.ajoutAchat(achat);
+				
+			}
+			response.sendRedirect("achats.jsp");
+			GestionMail.sendEmail(msg, to, sujet);
 			
 		}
 		
@@ -61,11 +75,6 @@ public class AjoutAchat extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
-	}
-	
-	
-	public static void ajoutAchat(Achat achat) {
-		
 	}
 
 }
